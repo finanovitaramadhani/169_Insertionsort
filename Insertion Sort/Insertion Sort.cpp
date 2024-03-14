f0885654144177a3b918118c68d4c96225a6bfde
finanovita <finanovita479@gmail.com>
2024-03-14 10:13:53 +0700

    Add project files.

---
+++ Insertion Sort/Insertion Sort.cpp
@@ -0,0 +1,9 @@
+// Insertion Sort.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int arr[20];			// Membuat Array dengan panjang data 20
+int n;					// Membuat Varuable inputan n
+
